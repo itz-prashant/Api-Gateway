1234dba189873052b66806d6d9b3774a99a0271d
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-08-29 18:41:07 +0530

    Added limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 const express = require('express')
-const {serverConfig, Logger} = require('./config')
+const rateLimit = require('express-rate-limit')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const {serverConfig} = require('./config')
 const apiRoutes = require('./routes')
 const app = express()
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes).
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter)
+
+app.use(
+    '/flightsService',
+    createProxyMiddleware({
+      target: serverConfig.FLIGHT_SERVICE,
+      changeOrigin: true,
+    }),
+  );
+app.use(
+    '/bookingService',
+    createProxyMiddleware({
+      target: serverConfig.BOOKING_SERVICE,
+      changeOrigin: true,
+      pathRewrite: {'^/bookingService' : '/'}
+    }),
+  );
 
 app.use('/api', apiRoutes)
 
